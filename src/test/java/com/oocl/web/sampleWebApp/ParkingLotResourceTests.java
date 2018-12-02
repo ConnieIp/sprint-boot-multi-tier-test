b399abd89d27f46120961a86eb2174633ae0312c
Connie Ip <connie.ip@cargosmart.com>
2018-12-02 11:13:24 +0800

    [Connie] feat: add ParkingLotResourceTests should_get_parking_boys

---
+++ src/test/java/com/oocl/web/sampleWebApp/ParkingLotResourceTests.java
@@ -0,0 +1,59 @@
+package com.oocl.web.sampleWebApp;
+
+import com.oocl.web.sampleWebApp.domain.ParkingBoy;
+import com.oocl.web.sampleWebApp.domain.ParkingLot;
+import com.oocl.web.sampleWebApp.domain.ParkingLotRepository;
+import com.oocl.web.sampleWebApp.models.ParkingBoyResponse;
+import com.oocl.web.sampleWebApp.models.ParkingLotResponse;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.persistence.EntityManager;
+
+import static com.oocl.web.sampleWebApp.WebTestUtil.getContentAsObject;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ParkingLotResourceTests {
+    @Autowired
+    ParkingLotRepository parkingLotRepository;
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void should_get_parking_boys() throws Exception {
+        //When GET /parkingLots, Return 200 with parking lots list [{"parkingLotID": "string", "availablePositionCount": integer, "capacity": integer}]
+
+        // Given
+        final ParkingLot lot = parkingLotRepository.save(new ParkingLot("lot",10));
+        parkingLotRepository.flush();
+
+        // When
+        final MvcResult result = mvc.perform(MockMvcRequestBuilders
+                .get("/parkinglots"))
+                .andReturn();
+
+        // Then
+        assertEquals(200, result.getResponse().getStatus());
+
+        final ParkingLotResponse[] parkingLots = getContentAsObject(result, ParkingLotResponse[].class);
+
+        assertEquals(1, parkingLots.length);
+        assertEquals("lot", parkingLots[0].getParkingLotId());
+    }
+
+}
